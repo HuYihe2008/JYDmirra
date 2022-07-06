a2425a04d0a58a494d504e28d06bfe62620f4498
HuYihe2008 <2812856215@qq.com>
2022-07-06 14:21:24 +0800

    新增自动复制index

---
+++ copy.py
@@ -0,0 +1,37 @@
+# srcfile 需要复制、移动的文件   
+# dstpath 目的地址
+
+#引用部分
+from glob import glob
+import os
+import shutil
+
+#后端函数部分
+def mycopyfile(srcfile,dstpath):                       # 复制函数
+    if not os.path.isfile(srcfile):
+        print ("%s not exist!"%(srcfile))
+    else:
+        fpath,fname=os.path.split(srcfile)             # 分离文件名和路径
+        if not os.path.exists(dstpath):
+            os.makedirs(dstpath)                       # 创建路径
+        shutil.copy(srcfile, dstpath + fname)          # 复制文件
+        print ("copy %s -> %s"%(srcfile, dstpath + fname))
+
+
+# 复制文件路径 
+
+src_dir = input("请输入复制文件目录：")                                  
+
+#获取文件|格式为"目录"+"文件名称"
+_name = input("请输入文件名称：")
+
+#执行获取文件
+src_file_list = glob(src_dir + '/' + _name)
+
+#执行文件目录遍历
+_path = input("请输入目录路径：")
+for root,dirs,files in os.walk(_path):
+        print(os.path.join(root))
+        #执行复制
+        for srcfile in src_file_list:
+            mycopyfile(srcfile, (root + '/')) 
